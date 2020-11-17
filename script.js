07adab2b2cffab58dfda365e06db6d63fa59c33a
Stas <botnarstasyan112002@gmail.com>
2020-11-17 21:03:22 +0200

    oper

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-const answer = prompt('List numbers');
-const arrOfNumbers = answer.split(',');
-console.log(arrOfNumbers); 
+const answer = prompt('Are you here');
+
+if (answer==null) {
+    console.log("Ok =(')");
+} else if(answer !=='-1') {
+    console.log('Not here');
+}
+
+console.log('Continue...')
