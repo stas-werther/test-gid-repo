2649ce1fd7d0d06fbf589d40b0ed9e4e5ffb8085
Stas <botnarstasyan112002@gmail.com>
2020-11-17 23:43:19 +0200

    yeap

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-let answer = prompt('Are you there?');
-let booleanAnswer = answer ==='false' ? false : true;
+let firtsNum = Number(prompt('Enter 1st number'));
+let secondNum = Number(prompt('Enter 2nd number'));
+let thirdNum = Number(prompt('Enter 3rd number'));
 
-if (answer ==='false') {
-    booleanAnswer = false;
-} else {
-    booleanAnswer = true;
-}
+let average = (firtsNum + secondNum + thirdNum)/3;
+console.log(average);
+;
---
+++ script2.js
@@ -0,0 +1,2 @@
+let YearUser = Number(prompt('Enter your year of birth'));
+console.log(2020 - YearUser);
